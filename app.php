72264414b1ff7e397a2324a23f1ad14f389abe21
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-19 13:47:27 +0000

    Initial commit - codeCademy project.

---
+++ app.php
@@ -0,0 +1,59 @@
+<!-- Practice everything you’ve learned so far about writing PHP programs. -->
+
+<?php
+  
+$annualExpenses = [
+    "vacations" => 1000,
+    "carRepairs" => 1000,    
+];
+
+$monthlyExpenses = [
+    "rent" => 1500,
+    "utilities" => 200,
+    "healthInsurance" => 200
+];
+
+$grossSalary = 48150;
+$socialSecurity = $grossSalary * .062;
+
+$incomeSegments = [[9700, .88], [29775, .78], [8675, .76]];
+
+// Write your code below:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
